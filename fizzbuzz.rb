66791be10257dd75d9a48837904cfd14698dcc46
amusing-bolt-9198 <edmund.nelson.b@gmail.com>
2019-01-08 06:35:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def 
+def fizzbuzz(num)
+  if (num%3 == 0 && num%5== 0  )
+    return "fizzbuzz"
+  elsif (num %3 == 0)
+    return 
+end
